83ac7fda02399e191a43286d243b8af2d14a523d
Leon <xiaoyankeji@hotmail.com>
2016-08-08 16:52:41 +0800

    第一提交

---
+++ XYButton/XYButton.Designer.vb
@@ -0,0 +1,66 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class XYButton
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl1 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(XYButton))
+        Me.ButtonImageList = New System.Windows.Forms.ImageList(Me.components)
+        Me.ButtonText = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ButtonImageList
+        '
+        Me.ButtonImageList.ImageStream = CType(resources.GetObject("ButtonImageList.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ButtonImageList.TransparentColor = System.Drawing.Color.Transparent
+        Me.ButtonImageList.Images.SetKeyName(0, "Button_N.png")
+        Me.ButtonImageList.Images.SetKeyName(1, "Button_P.png")
+        Me.ButtonImageList.Images.SetKeyName(2, "Button_FN.png")
+        '
+        'ButtonText
+        '
+        Me.ButtonText.ForeColor = System.Drawing.Color.White
+        Me.ButtonText.Location = New System.Drawing.Point(0, 0)
+        Me.ButtonText.Name = "ButtonText"
+        Me.ButtonText.Size = New System.Drawing.Size(140, 36)
+        Me.ButtonText.TabIndex = 0
+        Me.ButtonText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'XYButton
+        '
+        Me.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange
+        Me.BackColor = System.Drawing.Color.Transparent
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Controls.Add(Me.ButtonText)
+        Me.Name = "XYButton"
+        Me.Size = New System.Drawing.Size(140, 36)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ButtonImageList As System.Windows.Forms.ImageList
+    Friend WithEvents ButtonText As System.Windows.Forms.Label
+
+End Class
---
+++ XYButton/XYButton.vb
@@ -0,0 +1,56 @@
+﻿Public Class XYButton
+    Public Shadows Event Click(sender As Object, e As EventArgs)
+    Public Shadows Event MouseClick(sender As Object, e As MouseEventArgs)
+    Public Shadows Event MouseDown(sender As Object, e As MouseEventArgs)
+    Public Shadows Event MouseEnter(sender As Object, e As EventArgs)
+    Public Shadows Event MouseLeave(sender As Object, e As EventArgs)
+    Public Shadows Event MouseUp(sender As Object, e As MouseEventArgs)
+
+    Public Sub SetCaption(ByVal CaptionString As String)
+        ButtonText.Text = CaptionString
+    End Sub
+
+    Private Sub XiaoYanButton_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.BackgroundImage = ButtonImageList.Images(0)
+        ButtonText.Text = Me.Name
+    End Sub
+
+    Private Sub XYButton_Click(sender As Object, e As EventArgs) Handles ButtonText.Click
+        Me.BackgroundImage = ButtonImageList.Images(1)
+        RaiseEvent Click(sender, e)
+    End Sub
+
+    Private Sub XiaoYanButton_MouseClick(sender As Object, e As MouseEventArgs) Handles ButtonText.MouseClick
+        Me.BackgroundImage = ButtonImageList.Images(1)
+        RaiseEvent MouseClick(sender, e)
+    End Sub
+
+    Private Sub XiaoYanButton_MouseDown(sender As Object, e As MouseEventArgs) Handles ButtonText.MouseDown
+        Me.BackgroundImage = ButtonImageList.Images(2)
+        RaiseEvent MouseDown(sender, e)
+    End Sub
+
+    Private Sub XiaoYanButton_MouseEnter(sender As Object, e As EventArgs) Handles ButtonText.MouseEnter
+        Me.BackgroundImage = ButtonImageList.Images(1)
+        RaiseEvent MouseEnter(sender, e)
+    End Sub
+
+    Private Sub XiaoYanButton_MouseLeave(sender As Object, e As EventArgs) Handles ButtonText.MouseLeave
+        Me.BackgroundImage = ButtonImageList.Images(0)
+        RaiseEvent MouseLeave(sender, e)
+    End Sub
+
+    Private Sub XiaoYanButton_MouseUp(sender As Object, e As MouseEventArgs) Handles ButtonText.MouseUp
+        Me.BackgroundImage = ButtonImageList.Images(1)
+        RaiseEvent MouseUp(sender, e)
+    End Sub
+
+    Private Sub XiaoYanButton_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        Me.OnBackgroundImageChanged(e)
+        ButtonText.Size = Me.Size
+        'Me.Size = ButtonImageList.ImageSize
+    End Sub
+
+
+
+End Class
